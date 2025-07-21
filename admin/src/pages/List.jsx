b80e249167cf7fdb6c046f8cb1ccc05ae644a2e8
pratikwayase <pratikwayase@gmail.com>
2025-07-21 22:53:44 +0530

    /

--- admin/src/pages/List.jsx
+++ admin/src/pages/List.jsx
@@ -1,6 +1,27 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import { backendUrl } from '../App'
 
 const List = () => {
+
+  const [list, setList] = useState([])
+  
+  const fetchList = async () => {
+
+    try {
+
+      const response = await axios.get(backendUrl + '/api/product/list')
+      setList(response.data)
+
+    } catch (error) {
+      
+    }
+  }
+
+  useEffect(() => {
+  
+},[])
+
   return (
     <div>
       
