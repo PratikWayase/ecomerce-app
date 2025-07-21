c80969bac3c5779943762b78088bf2b2b1efc828
pratikwayase <pratikwayase@gmail.com>
2025-07-21 16:22:15 +0530

    /

--- admin/src/pages/Add.jsx
+++ admin/src/pages/Add.jsx
@@ -3,6 +3,7 @@ import { Form } from 'react-router-dom'
 import { assets } from '../assets/assets'
 import axios from 'axios'
 import {backendUrl} from '../App'
+import { toast } from 'react-toastify'
 
 
 
@@ -50,10 +51,26 @@ const Add = ({token}) => {
       
       const response = await axios.post(backendUrl + "/api/product/add", fromData,{headers:{token}})
       
-      console.log(response.data)
+      if (response.data.success) {
+
+        toast.success(response.data.message)
+        setName('')
+        setDescription('')
+        setImage1(false)
+        setImage2(false)
+        setImage3(false)
+        setImage4(false)
+        setPrice('')
+      }
+      else {
+        toast.error(response.data.message)
+       }
       
 
     } catch (error) {
+
+      console.log(error)
+      toast.error(error.message)
       
     }
 
